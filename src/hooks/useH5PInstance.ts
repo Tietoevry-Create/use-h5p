1a503d8b8529899ab00779272a5669c884fc7c87
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-16 15:36:27 +0200

    generalize `useH5PInstance`

--- src/hooks/useH5PInstance.ts
+++ src/hooks/useH5PInstance.ts
@@ -1,7 +1,9 @@
 import type { IH5PContentType } from "h5p-types";
-import { useContext } from "react";
+import { useContext, Context } from "react";
 import { H5PContext } from "../contexts/H5PContext";
 
-export const useH5PInstance = (): IH5PContentType | undefined => {
-  return useContext(H5PContext);
+export const useH5PInstance = <
+  TH5PContentType extends IH5PContentType = IH5PContentType,
+>(): TH5PContentType | undefined => {
+  return useContext<TH5PContentType>(H5PContext as Context<TH5PContentType>);
 };
