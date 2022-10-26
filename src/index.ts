a5591c3c82d7ee93abdd1efc664a000c70111451
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-10-26 10:51:05 +0200

    feat: export new hook

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,6 @@ export { useLocalization, useL10n } from "./hooks/useLocalization";
 export { useLocalizations, useL10ns } from "./hooks/useLocalizations";
 export { useH5PInstance } from "./hooks/useH5PInstance";
 export { useContentId } from "./hooks/useContentId";
+export { useTranslation } from "./hooks/useTranslation";
 
 export { Translations } from "./types/Translations";
