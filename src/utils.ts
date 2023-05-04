a16842d10d482cee4aad8e2539933422bbd8d2f7
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-05-04 17:59:15 +0200

    chore: fix format

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,2 @@
-export const getFallbackString = (translationKey: string): string => `[Missing translation: ${translationKey}]`
+export const getFallbackString = (translationKey: string): string =>
+  `[Missing translation: ${translationKey}]`;
