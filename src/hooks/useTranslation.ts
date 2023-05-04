a6c16b54493e57ecd9754f27171a4cf5a6eaf88b
Sindre Bøyum <boyum@users.noreply.github.com>
2023-05-04 19:06:08 +0200

    feat(use-translation): export `tOpts` to easily translate with options (#5)

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,14 +1,34 @@
+import type { TranslationParams } from "h5p-types";
 import { useContext } from "react";
 import { L10nContext } from "../contexts/LocalizationContext";
 import { getFallbackString } from "../utils";
 
-export const useTranslation = <TranslationKey extends string = string>(): {
-  t: (key: TranslationKey) => string;
-} => {
+export const useTranslation = <TranslationKey extends string = string>() => {
   const translations = useContext(L10nContext);
 
+  type Translations = typeof translations;
+
   return {
-    t: (translationKey: TranslationKey): string =>
-      translations?.[translationKey] ?? getFallbackString(translationKey),
+    t: (translationKey: TranslationKey): string => {
+      const translation =
+        translations?.[translationKey] ?? getFallbackString(translationKey);
+
+      return translation;
+    },
+    tOpts: (
+      translationKey: TranslationKey,
+      opts: TranslationParams<Translations[TranslationKey]>,
+    ): string => {
+      const translation =
+        translations?.[translationKey] ?? getFallbackString(translationKey);
+
+      const options: Array<[string, string]> = Object.entries(opts);
+
+      return options.reduce((acc, [key, value]) => {
+        const regex = new RegExp(key, "g");
+
+        return acc.replace(regex, value);
+      }, translation);
+    },
   };
 };
